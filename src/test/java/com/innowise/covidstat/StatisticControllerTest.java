d590bf194bf99e129fd5a154dafe88b8dbbaca79
evensky1 <fromthedeepsize@gmail.com>
2023-05-02 12:18:53 +0300

    Add tests

--- src/test/java/com/innowise/covidstat/StatisticControllerTest.java
+++ src/test/java/com/innowise/covidstat/StatisticControllerTest.java
@@ -1,12 +1,11 @@
 package com.innowise.covidstat;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.innowise.covidstat.model.CountryStatistic;
 import com.innowise.covidstat.model.StatisticRequest;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
@@ -18,14 +17,13 @@ import org.junit.jupiter.api.Test;
 class StatisticControllerTest {
 
     @Test
-    void createStatisticTest() throws JsonProcessingException {
+    void createStatisticTest() {
 
         var from = Instant.parse("2023-03-01T00:00:00.00Z");
         var to = Instant.parse("2023-03-02T00:00:00.00Z");
 
         var requestEntity = new StatisticRequest(List.of("lithuania"), from, to);
 
-        var expectedEntity = List.of(new CountryStatistic("Lithuania", 774, to, 43, from));
         var mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
 
@@ -36,7 +34,56 @@ class StatisticControllerTest {
                 .post("/api/stat")
             .then()
                 .statusCode(201)
+                .body("country", contains("Lithuania"))
+                .body("maxNewCases", contains(380))
+                .body("maxNewCasesDate", contains("2023-03-01T00:00:00Z"))
+                .body("minNewCases", contains(359))
+                .body("minNewCasesDate", contains("2023-03-02T00:00:00Z"));
+    }
+
+    @Test
+    void emptyRequestTest() {
+
+        var from = Instant.parse("2023-03-01T00:00:00.00Z");
+        var to = Instant.parse("2023-03-02T00:00:00.00Z");
+
+        var requestEntity = new StatisticRequest(List.of(), from, to);
+
+        var mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        given()
+            .when()
+                .body(requestEntity)
                 .contentType(ContentType.JSON)
-                .body(is(mapper.writeValueAsString(expectedEntity)));
+            .post("/api/stat")
+                .then()
+                .statusCode(201)
+                .body("", empty());
+    }
+
+    @Test
+    void corruptedCountryNameTest() {
+
+        var from = Instant.parse("2023-03-01T00:00:00.00Z");
+        var to = Instant.parse("2023-03-02T00:00:00.00Z");
+
+        var requestEntity = new StatisticRequest(List.of("lithuania", "nonexisting"), from, to);
+
+        var mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        given()
+            .when()
+                .body(requestEntity)
+                .contentType(ContentType.JSON)
+                .post("/api/stat")
+            .then()
+                .statusCode(201)
+                .body("country", contains("Lithuania"))
+                .body("maxNewCases", contains(380))
+                .body("maxNewCasesDate", contains("2023-03-01T00:00:00Z"))
+                .body("minNewCases", contains(359))
+                .body("minNewCasesDate", contains("2023-03-02T00:00:00Z"));
     }
 }
